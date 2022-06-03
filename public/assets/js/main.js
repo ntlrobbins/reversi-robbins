e325cd65c9a2d354a3ca15f710de37b69fa51706
Natalie Robbins <nsrobbin@uci.edu>
2022-06-02 23:10:53 -0400

    Added final touches.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -215,7 +215,7 @@ socket.on('player_disconnected', (payload) => {
 	}
 
 
-	let newHTML = '<p class=\'left_room_response\'>' + payload.username + ' left the ' + payload.room + '. (There are ' + payload.count + ' users in this room)</p>';
+	let newHTML = '<p class=\'left_room_response\'>' + payload.username + ' left the chatroom. (There are ' + payload.count + ' users in this room)</p>';
 	let newNode = $(newHTML);
 	newNode.hide();
 	$('#messages').prepend(newNode);
@@ -251,17 +251,18 @@ socket.on('send_chat_message_response', (payload) =>{
 });
 
 let old_board = [
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?'],
-	['?', '?', '?', '?', '?', '?', '?', '?']
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+	[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 	];
 
 	let my_color = "";
+	let interval_timer;
 
 socket.on('game_update', (payload) => {
 	if(( typeof payload == 'undefined') || (payload === null)){
@@ -397,6 +398,34 @@ let blacksum = 0;
 				}
 			}
 		}
+
+		clearInterval(interval_timer)
+		interval_timer = setInterval( ((last_time) => {
+			return ( () => {
+				let d = new Date();
+				let elapsed_m = d.getTime() - last_time;
+				let minutes = Math.floor(elapsed_m / (60 * 1000));
+				let seconds = Math.floor((elapsed_m % (60 * 1000)) / 1000);
+				let total = minutes * 60 + seconds;
+				if (total > 100) {
+					total = 100;
+				}
+				$("#elapsed").css("width",total+"%").attr("aria-valuenow",total);
+				let timestring = "" + seconds;
+				timestring = timestring.padStart(2, '0');
+				timestring = minutes + ":" + timestring;
+				if (total < 100) {
+					$("#elapsed").html(timestring);
+				}
+				else {
+					$("#elapsed").html("Times up!");
+				}
+			})
+		})(payload.game.last_move_time)
+		, 1000);
+
+
+
 	$("#whitesum").html(whitesum);
 	$("#blacksum").html(blacksum);
 	old_board = board;
--- server.js
+++ server.js
@@ -580,6 +580,9 @@ socket.on('play_token', (payload) => {
 				game.legal_moves = calculate_legal_moves('w',game.board);
 			}
 
+			let d = new Date();
+			game.last_move_time = d.getTime();
+
 			send_game_update(socket, game_id, 'played a token');
 	});
 });
